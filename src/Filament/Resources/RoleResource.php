749f07a909fb3c1206bea0e848787953b3960c6b
Marcus <marcus.venghaus@inklammern.de>
2024-03-12 13:40:23 +0100

    wip

--- src/Filament/Resources/RoleResource.php
+++ src/Filament/Resources/RoleResource.php
@@ -63,6 +63,7 @@ class RoleResource extends Resource
                                 )
                                 ->required()
                         ])
+                        ->columns()
                 ])
         ];
     }
